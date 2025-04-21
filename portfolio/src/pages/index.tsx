572c97108ade944c922d1827a6115a71201aab41
vquijano12 <vquijano7224@gmail.com>
2025-04-21 11:52:34 -0400

    refactor: migrate HTML to JSX

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <a
+        href="https://www.linkedin.com/in/vquijano7224/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn
+      </a>
+      <a
+        href="https://github.com/vquijano12"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
+    </div>
+  );
 }
