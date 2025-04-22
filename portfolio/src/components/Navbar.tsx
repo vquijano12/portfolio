95d88ff7ac78fd423fa1ff3e33499a11c93f455b
vquijano12 <vquijano7224@gmail.com>
2025-04-21 23:16:58 -0400

    style: add styling to Navbar

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -3,15 +3,17 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav className="p-2">
+      <ul className="flex space-x-2 mb-2">
         <li>
           <Link href="/">Home Page</Link>
         </li>
+        <li>|</li>
         <li>
           <Link href="/html-practice">HTML Practice 1</Link>
         </li>
       </ul>
+      <hr />
     </nav>
   );
 }
