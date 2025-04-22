2fe27fdeec8cf5d522a335584dc8296c5a145dab
vquijano12 <vquijano7224@gmail.com>
2025-04-21 23:22:06 -0400

    refactor: remove target and rel attributes from external links

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -2,21 +2,9 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <a
-        href="https://www.linkedin.com/in/vquijano7224/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        LinkedIn
-      </a>
-      <a
-        href="https://github.com/vquijano12"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub
-      </a>
+    <div className="p-2 flex space-x-2">
+      <a href="https://www.linkedin.com/in/vquijano7224/">LinkedIn</a>
+      <a href="https://github.com/vquijano12">GitHub</a>
     </div>
   );
 }
